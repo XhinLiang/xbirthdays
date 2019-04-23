8c780114820e55659e29c26ffb9891f5b4f622e8
xhinliang <xhinliang@gmail.com>
2019-04-23 20:59:30 +0800

    rm useless import

--- src/main/java/com/xhinliang/birthdays/MainApp.java
+++ src/main/java/com/xhinliang/birthdays/MainApp.java
@@ -10,7 +10,6 @@ import javax.annotation.Nullable;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.springframework.beans.factory.NoSuchBeanDefinitionException;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
